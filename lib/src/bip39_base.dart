453eb36388ef46026fc263b8d4be2925fa9335c4
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-07-31 19:48:44 +0900

    generate mnemonic

--- lib/src/bip39_base.dart
+++ lib/src/bip39_base.dart
@@ -1,6 +1,12 @@
 import 'dart:math';
 import 'dart:typed_data';
+import 'package:crypto/crypto.dart';
+import 'package:resource/resource.dart';
+import 'dart:convert';
+import 'dart:async';
 
+//import 'package:pointycastle/random/auto_seed_block_ctr_random.dart';
+//import 'package:pointycastle/block/modes/ctr.dart';
 
 // https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
 // https://github.com/bitcoin/bips/tree/master/bip-0039
@@ -19,32 +25,110 @@ enum Wordlist {
 
 const Wordlist _DEFAULT_WORDLIST = Wordlist.ENGLISH;
 
-ByteData mnemonicToEntropy(String mnemonic, Wordlist wordlist) {
+const int _SIZE_8BITS = 255;
+const String _INVALID_ENTROPY = 'Invalid entroy';
+const String _INVALID_MNEMONIC = 'Invalid mnemonic';
+const String _INVALID_CHECKSUM = 'Invalid checksum';
 
+String _getWordlistName(Wordlist wordlist) {
+  switch (wordlist) {
+    case Wordlist.CHINESE_SIMPLIFIED:
+      return 'chinese_simplified';
+    case Wordlist.CHINESE_TRADITIONAL:
+      return 'chinese_traditional';
+    case Wordlist.ENGLISH:
+      return 'english';
+    case Wordlist.FRENCH:
+      return 'french';
+    case Wordlist.ITALIAN:
+      return 'italian';
+    case Wordlist.JAPANESE:
+      return 'japanese';
+    case Wordlist.KOREAN:
+      return 'korean';
+    case Wordlist.SPANISH:
+      return 'spanish';
+    default:
+      return 'english';
+  }
 }
 
-String entropyToMnemonic(ByteData entropy, Wordlist wordlist) {
-
+Uint8List _nextBytes(int size) {
+  final rnd = Random.secure();
+  final bytes = Uint8List(size);
+  for (var i = 0; i < size; i++) {
+    bytes[i] = rnd.nextInt(_SIZE_8BITS);
+  }
+  return bytes;
 }
 
-String generateMnemonic({
+int _binaryToByte(String binary) {
+  return int.parse(binary, radix: 2);
+}
+
+String _bytesToBinary(Uint8List bytes) {
+  return bytes.map((byte) => byte.toRadixString(2).padLeft(8, '0')).join('');
+}
+
+String deriveChecksumBits(Uint8List entropy) {
+  final ENT = entropy.length * 8;
+  final CS = ENT ~/ 32;
+
+  final hash = sha256.convert(entropy);
+  return _bytesToBinary(Uint8List.fromList(hash.bytes)).substring(0, CS);
+}
+
+// ByteData mnemonicToEntropy(String mnemonic, Wordlist wordlist) {}
+
+Future<String> entropyToMnemonic(Uint8List entropy, Wordlist wordlist) async {
+  if (entropy.length < 16) {
+    throw ArgumentError(_INVALID_ENTROPY);
+  }
+  if (entropy.length > 32) {
+    throw ArgumentError(_INVALID_ENTROPY);
+  }
+  if (entropy.length % 4 != 0) {
+    throw ArgumentError(_INVALID_ENTROPY);
+  }
+
+  final entroypyBits = _bytesToBinary(entropy);
+  final checksumBits = deriveChecksumBits(entropy);
+
+  final bits = entroypyBits + checksumBits;
+
+  final regex = new RegExp(r".{1,11}", caseSensitive: false, multiLine: false);
+  final chunks = regex
+      .allMatches(bits)
+      .map((match) => match.group(0))
+      .toList(growable: false);
+
+  final wordRes = await _loadWordlist(wordlist);
+
+  return chunks
+      .map((binary) => wordRes[_binaryToByte(binary)])
+      .join(wordlist == Wordlist.JAPANESE ? '\u3000' : ' ');
+}
+
+Future<String> generateMnemonic({
   int strength = 128,
   Wordlist wordlist = _DEFAULT_WORDLIST,
-}){
+}) async {
   assert(strength % 32 == 0);
 
-  final random = Random.secure();
+  final entropy = _nextBytes(strength ~/ 8);
+
+  return await entropyToMnemonic(entropy, wordlist);
 }
 
-bool validateMnemonic(String mnemonic, Wordlist wordlist) {
+// bool validateMnemonic(String mnemonic, Wordlist wordlist) {}
 
-}
-
-List<String> _loadWordlist(Wordlist wordlist) {
-
-}
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+Future<List<String>> _loadWordlist(Wordlist wordlist) async {
+  final res =
+      Resource('package:bip39/src/wordlists/${_getWordlistName(wordlist)}.txt');
+  final rawWords = await res.readAsString(encoding: utf8);
+  return rawWords
+      .split('\n')
+      .map((s) => s.trim())
+      .where((s) => s.isNotEmpty)
+      .toList(growable: false);
 }
--- test/bip39_test.dart
+++ test/bip39_test.dart
@@ -1,16 +1,66 @@
-import 'package:bip39/bip39.dart';
+import 'package:bip39/bip39.dart' as bip39;
 import 'package:test/test.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'dart:typed_data';
 
 void main() {
+  Map<String, dynamic> vectors =
+      json.decode(File('./test/vectors.json').readAsStringSync(encoding: utf8));
+
   group('A group of tests', () {
-    Awesome awesome;
+    setUp(() async {});
 
-    setUp(() {
-      awesome = new Awesome();
+    // testVector('English', bip39.Wordlist.ENGLISH, '', vectors['english'][0], 0);
+    int i = 0;
+    (vectors['english'] as List<dynamic>).forEach((list) {
+      testVector('English', bip39.Wordlist.ENGLISH, '', list, i);
+      i++;
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    i = 0;
+    (vectors['japanese'] as List<dynamic>).forEach((list) {
+      testVector('Japanese', bip39.Wordlist.JAPANESE, '', list, i);
+      i++;
     });
+
+    // test('First Test', () async {
+    //   // await bip39.entropyToMnemonic(entropy, wordlist)
+    //   final mnemonic = await bip39.generateMnemonic();
+    //   print("mnemonic: $mnemonic");
+    // });
   });
+}
+
+void testVector(String description, bip39.Wordlist wordlist, String password,
+    List<dynamic> v, int i) {
+  final ventropy = v[0];
+  final vmnemonic = v[1];
+  final vseedHex = v[2];
+
+  test('for ${description}(${i}), ${ventropy}', () async {
+    final regex = new RegExp(r".{1,2}", caseSensitive: false, multiLine: false);
+    final entropy = Uint8List.fromList(regex
+        .allMatches(ventropy)
+        .map((s) => int.parse(s.group(0), radix: 16))
+        .toList(growable: false));
+
+    print('entropy: $entropy');
+
+    final code = await bip39.entropyToMnemonic(entropy, wordlist);
+    expect(code, equals(vmnemonic));
+
+    // print('code: $code');
+  });
+}
+
+List<String> loadWordlist(String name) {
+  final raw =
+      File('./lib/src/wordlists/${name}.txt').readAsStringSync(encoding: utf8);
+  return raw
+      .split('\n')
+      .map((s) => s.trim())
+      .where((s) => s.isNotEmpty)
+      .toList(growable: false);
 }
