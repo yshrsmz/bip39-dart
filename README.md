a871cf275c37ae89641cd6c69feb1edff133ab88
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-08-21 19:38:06 +0900

    update example

--- README.md
+++ README.md
@@ -8,11 +8,29 @@ Dart implementation of [Bitcoin BIP39](https://github.com/bitcoin/bips/blob/mast
 
 A simple usage example:
 
-    import 'package:bip39/bip39.dart';
+```dart
+import 'package:bip39/bip39.dart' as bip39;
 
-    main() {
-      var awesome = new Awesome();
-    }
+main() {
+  // Generate a random mnemonic (uses Random.secure() under the hood), defaults to 128-bits of entropy
+  var mnemonic = await bip39.generateMnemonic();
+  // => 'alarm boost mom torch couple owner myself gift sugar tell ticket panther'
+  
+  bip39.mnemonicToSeedHex('basket actual');
+  // => '5cf2d4a8b0355e90295bdfc565a022a409af063d5365bb57bf74d9528f494bfa4400f53d8349b80fdae44082d7f9541e1dba2b003bcfec9d0d53781ca676651f'
+  
+  bip39.mnemonicToSeed('basket actual');
+  // => [92, 242, 212, 168, 176, 53, 94, 144, 41, 91, 223, 197, 101, 160, 34, 164, 9, 175, 6, 61, 83, 101, 187, 87, 191, 116, 217, 82, 143, 73, 75, 250, 68, 0, 245, 61, 131, 73, 184, 15, 218, 228, 64, 130, 215, 249, 84, 30, 29, 186, 43, 0, 59, 207, 236, 157, 13, 83, 120, 28, 166, 118, 101, 31]
+  
+  await bip39.validateMnemonic(mnemonic);
+  // => true
+  
+  await bip39.validateMnemonic('basket actual');
+  // => false
+  
+  
+}
+```
 
 ## Features and bugs
 
--- example/bip39_example.dart
+++ example/bip39_example.dart
@@ -1,23 +1,26 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:bip39/bip39.dart' as bip39;
-import 'package:pointycastle/pointycastle.dart';
 
 String _bytesToHex(Uint8List bytes) {
   return bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join('');
 }
 
 main() async {
-  final sha256 = Digest("SHA-256");
-  final sha512 = Digest("SHA-512");
-  final sha256result = sha256.process(utf8.encode('123'));
-  final sha512result = sha512.process(utf8.encode('123'));
-//  print('sha256: ${sha256.convert(utf8.encode('123'))}');
-//  print('sha512: ${sha512.convert(utf8.encode('123'))}');
-  print('sha256: ${_bytesToHex(sha256result)}');
-  print('sha512: ${_bytesToHex(sha512result)}');
-
   final mnemonic = await bip39.generateMnemonic();
   print('mnemonic code: ${mnemonic}');
+
+  print(bip39.mnemonicToSeedHex('basket actual'));
+
+  print(bip39.mnemonicToSeed('basket actual'));
+
+  // defaults to BIP39 English word list
+  // uses HEX strings for entropy
+  var mnemonic2 =
+      await bip39.entropyHexToMnemonic('00000000000000000000000000000000');
+  // => abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about
+
+  // reversible
+  bip39.mnemonicToEntropy(mnemonic2);
+  // => '00000000000000000000000000000000'
 }
